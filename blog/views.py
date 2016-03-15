2b1e0c59a841f13ef747de5c108b4bf05afdd55f
Olivier Wenzek <olivier.wenzek@gmail.com>
2016-03-15 22:49:21 +0100

    02 - Updated urls routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+	text = """<h1>Bienvenue sur le blog</h1><p>J'aime bien chercher des annonces et les comparer</p>"""
+
+	return HttpResponse(text)
