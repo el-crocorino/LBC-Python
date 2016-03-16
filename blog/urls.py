a8f836d6a99e1350a110fb3813c553711b0b0d28
Olivier Wenzek <olivier.wenzek@gmail.com>
2016-03-16 13:38:08 +0100

    02 - Added article view w/ param

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('blog.views',
     url(r'^accueil/$', 'home'),
+    url(r'^article/(\d+)/$', 'article_view'),
+    url(r'^article/(\d{4})/(\d{2})/$', 'article_list'),
 	)
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,11 @@ def home(request):
 	text = """<h1>Bienvenue sur le blog</h1><p>J'aime bien chercher des annonces et les comparer</p>"""
 
 	return HttpResponse(text)
+
+def article_view(request, article_id):
+	"""Displays article with given id"""
+	text = """Vous avez demandé l'article # {0}.""".format(article_id)
+	
+	return HttpResponse(text)
+
+
