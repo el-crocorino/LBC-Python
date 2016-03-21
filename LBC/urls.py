7ab869bc4d4a514215e1d66bfa7dad132ebca362
Olivier Wenzek <olivier.wenzek@gmail.com>
2016-03-21 22:22:47 +0100

    feature/03 - renamed app from lbc_ads to app

--- LBC/urls.py
+++ LBC/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls'))
+    url(r'^blog/', include('blog.urls')),
+    url(r'^app/', include('app.urls')),
 )
