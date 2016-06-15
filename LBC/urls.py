62c8a607e483300c36b13dacc65ef819f8675f0f
el-crocorino <owenzek@gmail.com>
2016-06-15 07:51:04 +0200

    Updated urls.py files to handle django 1.10 specs

--- LBC/urls.py
+++ LBC/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = ['',
     # Examples:
     # url(r'^$', 'LBC.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -9,4 +9,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
     url(r'^app/', include('app.urls')),
-)
+]
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-urlpatterns = patterns('blog.views',
+urlpatterns = ['blog.views',
     url(r'^accueil/$', 'home'),
     url(r'^article/(?P<article_id>\d+)/$', 'article_view'),
     url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/$', 'article_list'),
     url(r'^redirection/$', 'redirection_view'),
     url(r'^$', 'tpl')
-	)
+]
