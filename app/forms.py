c746adaedd2d5eab23a531df9e826f252ad7a66c
el-crocorino <owenzek@gmail.com>
2016-08-03 13:31:09 +0200

    feature/07.1 - Didn't use formsets for rummae items, implemented not rummage item form

--- app/forms.py
+++ app/forms.py
@@ -9,6 +9,8 @@ class RummageAddForm (forms.Form):
     title = forms.CharField(max_length=100, label=u"Nom :", required=True)
     url = forms.CharField(label=u"URL : ", help_text=u"Collez ici l'adresse de votre page de recherche", required=True)   
     
+    
+    
 class CriteriaAddForm (ModelForm):
 
     class Meta:
@@ -26,28 +28,42 @@ class CriteriaAddForm (ModelForm):
             })
         }        
     
-class Rummage_itemAddForm (ModelForm):    
-#class Rummage_itemAddForm (forms.Form):    
-    ##rummage_id = forms.IntegerField(max_length=10, label=u"Id recherche", required=True)
-    #selected = forms.BooleanField()
-    #lbc_id = forms.IntegerField()    
-    #name = forms.CharField(max_length=200)
-    ##url = forms.CharField()
-    ##thumbnail_url = forms.CharField()
-    #price = forms.CharField()
-    ##infos = forms.CharField()   
+#class Rummage_itemAddForm (ModelForm):    
+class Rummage_itemAddForm (forms.Form):    
+    rummage_id = forms.IntegerField(label=u"Id recherche", required=True)
+    selected = forms.BooleanField()
+    lbc_id = forms.IntegerField()    
+    name = forms.CharField(max_length=200)
+    url = forms.CharField()
+    thumbnail_url = forms.CharField()
+    price = forms.CharField()
+    infos = forms.CharField()   
     
-    class Meta:
-        model = Rummage_item
-        exclude = ('rummage', 'created_date','updated_date')  
-        labels = {
-            "name": "Nom :",
-            "weight": "Poids (0,1; 0,3; ...) :"
-        }        
-        widgets = {
-            'weight': widgets.NumberInput( attrs = {
-                'step' : '0.01',
-                'min' : '0.0',
-                'max' : '1.0',
-            })
-        }      
+    #class Meta:
+        #model = Rummage_item
+        #exclude = ('rummage', 'created_date','updated_date')  
+        #labels = {
+            #"name": "Nom :",
+            #"price": "Prix:"
+        #}        
+        #widgets = {
+            #'weight': widgets.NumberInput( attrs = {
+                #'step' : '0.01',
+                #'min' : '0.0',
+                #'max' : '1.0',
+            #})
+        #}  
+        
+class ConfForm(forms.Form):
+    """
+    Formulaire de configuration du module
+    """
+    format_x = forms.IntegerField( label="Largeur", required=True, min_value=0, widget=forms.TextInput(attrs={'placeholder':'mm','class':'form-control input-sm'}) )
+    format_y = forms.IntegerField( label="Hauteur", required=True, min_value=0, widget=forms.TextInput(attrs={'placeholder':'mm','class':'form-control input-sm'}) )
+    
+class SorteForm(forms.Form):
+
+    #Champs multiples
+    designation = forms.CharField( label="Désignation", required=True, widget=forms.TextInput(attrs={'placeholder':'','class':'form-control input-sm'}) )
+    quantite = forms.IntegerField( label="Quantité", required=True, min_value=0, widget=forms.TextInput(attrs={'placeholder':'','class':'form-control input-sm'}) )
+    rectoverso = forms.ChoiceField(label='',choices = [(False,'Recto Seul'),(True,'Recto Verso')],required=True,widget=forms.Select( attrs={'class':'form-control input-sm'}))
